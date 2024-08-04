28490380ecbca4eb47ab7b3b56870b301115d5d4
Gniewko Świątek <80861162+Fzhut0@users.noreply.github.com>
2024-08-04 16:34:50 +0200

    Additional cleanup

--- AFSTest/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ AFSTest/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using AFSTest.Data;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
--- AFSTest/Repositories/TranslationRepository.cs
+++ AFSTest/Repositories/TranslationRepository.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using AFSTest.Data;
 using AFSTest.Models;
 using AFSTest.Repositories.Interfaces;
